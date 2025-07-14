2415d39e9d2158ea030a438645669699d9658a73
Henrique J. Araujo <liderhenrique@gmail.com>
2025-07-14 20:19:10 -0300

    conf: adicionando configuração do firebase

---
+++ src/app/lib/firebase.ts
@@ -0,0 +1,34 @@
+import "server-only"
+
+import { cert, getApps, initializeApp } from "firebase-admin/app"
+import { getFirestore } from "firebase-admin/firestore"
+import { getStorage } from "firebase-admin/storage"
+
+
+// Certificado
+
+if (!process.env.FIREBASE_PRIVATE_KEY_BASE64) {
+  throw new Error('Variable ambient not exists')
+}
+
+const decodedKey = Buffer.from(process.env.FIREBASE_PRIVATE_KEY_BASE64, "base64").toString("utf-8")
+
+export const firebaseCert = cert({
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: decodedKey
+})
+
+// Instancia do App
+
+if (getApps().length === 0) {
+  initializeApp({
+    credential: firebaseCert,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+  })
+}
+
+export const database = getFirestore()
+
+export const storage = getStorage().bucket()
+
